f42df02f23a85657fa38a63e00cf816058fcca45
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-07-26 21:53:22 -0300

    Removendo todo o código para ser refatorado do extremo 0

--- ToDoAPI.NET/ToDoAPI.NET/Program.cs
+++ ToDoAPI.NET/ToDoAPI.NET/Program.cs
@@ -1,9 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using ToDoAPI.NET.Context;
-using ToDoAPI.NET.Interfaces;
-using ToDoAPI.NET.Model.Repositories;
-using ToDoAPI.NET.Validations.Interfaces;
-using ToDoAPI.NET.Validations.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,13 +8,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<AppDbContext>(op =>
-            op.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
-            b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
-
-builder.Services.AddScoped<IJobRepository, JobRepository>();
-builder.Services.AddScoped<IValidateStatusJobs, ValidateStatusJobs>();
 
 var app = builder.Build();
 
